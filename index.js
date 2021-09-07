093a222d39f50d6db43bb0e6d3e0bb580ddd879c
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-07 23:12:49 +0900

    맛보기

---
+++ index.js
@@ -0,0 +1,9 @@
+var value = 'kim'; // 변수를 선언할 때 타입을 선언해줘야함
+var arr = ['hello', 'hi', 'Teemo'];
+var obj = { name: "teemo" };
+var twotype = 1234;
+var Teemo = [123, true];
+function func(x) {
+    return x * 2;
+}
+console.log(func(12));
---
+++ index.ts
@@ -0,0 +1,14 @@
+let value:string = 'kim';// 변수를 선언할 때 타입을 선언해줘야함
+let arr:string[] = ['hello', 'hi', 'Teemo'];
+let obj:{name: string} = {name: "teemo"};
+let twotype: string | number = 1234;
+
+type Member = [number, boolean];
+
+let Teemo:Member = [123, true];
+
+function func(x: number) : number{
+    return x * 2;
+}
+
+console.log(func(12))
