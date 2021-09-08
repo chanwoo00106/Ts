a89bcee9340582530346ccc35a4d2fe2c955d5b6
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-08 22:15:52 +0900

    ts class 연습

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-var information = {
-    name: 'kim',
-    age: 30,
-    plusOne: function (x) {
-        return x + 1;
-    },
-    changeName: function (say) {
-        console.log(say);
+var Car = /** @class */ (function () {
+    function Car(model, price) {
+        this.model = model;
+        this.price = price;
     }
-};
-console.log(information.plusOne(1));
-information.changeName('안녕');
+    Car.prototype.tax = function () {
+        return this.price / 10;
+    };
+    return Car;
+}());
+var car1 = new Car('소나타', 3000);
+console.log(car1); //콘솔창 출력결과는 { model : '소나타', price : 3000 }
+console.log(car1.tax()); //콘솔창 출력결과는 300
+var Word = /** @class */ (function () {
+    function Word() {
+        var param = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            param[_i] = arguments[_i];
+        }
+        var nums = [];
+        var strs = [];
+        param.forEach(function (i) {
+            if (typeof i === 'number')
+                nums.push(i);
+            else
+                strs.push(i);
+        });
+        this.num = nums;
+        this.str = strs;
+    }
+    return Word;
+}());
+var obj = new Word('kim', 3, 5, 'park');
+console.log(obj.num); //[3,5]
+console.log(obj.str); //['kim', 'park']
--- index.ts
+++ index.ts
@@ -1,12 +1,39 @@
-let information = {
-    name : 'kim',
-    age : 30,
-    plusOne (x :number) :number{
-      return x + 1
-    },
-    changeName : (say :string):void => {
-      console.log(say);
+class Car {
+    model;
+    price;
+    constructor(model:string, price:number){
+        this.model = model;
+        this.price = price;
+    }
+
+    tax() {
+        return  this.price / 10;
     }
 }
-console.log(information.plusOne(1));
-information.changeName('안녕');
+
+let car1 = new Car('소나타', 3000)
+console.log(car1) //콘솔창 출력결과는 { model : '소나타', price : 3000 }
+console.log(car1.tax()) //콘솔창 출력결과는 300
+
+
+class Word {
+    str;
+    num;
+    constructor(...param){
+        let nums:number[] =[];
+        let strs:string[] =[];
+
+        param.forEach(i => {
+            if (typeof i === 'number') nums.push(i);
+            else strs.push(i);
+        });
+
+        this.num = nums;
+        this.str = strs;
+    }
+}
+
+
+let obj = new Word('kim', 3, 5, 'park');
+console.log(obj.num) //[3,5]
+console.log(obj.str) //['kim', 'park']
