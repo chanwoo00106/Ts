105fc1a8630c9959b7772e19c2152c2d3772fb5a
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-09 23:09:35 +0900

    axios 사용

--- tsreact/src/App.tsx
+++ tsreact/src/App.tsx
@@ -1,24 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+
+interface data {
+  [key: string]: string | number | data;
+}
 
 function App() {
   const [user, setUser] = useState('teemo');
+  const [input, setInput] = useState("");
+  const [data, setData] = useState([]);
 
-  const changeName = ():void => {
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(({ data }) => {
+        setData(data);
+      })
+  })
+
+  const changeName = (): void => {
     if (user === 'teemo') setUser('cute');
     else setUser('teemo');
   }
-  
+
+  const onChange = (e: { target: { value: string } }) => setInput(e.target.value);
+
   return (
     <div className="App">
-      <p>{user}</p>
+      <h1>{user}</h1>
       <button onClick={changeName}>Change</button>
+      <h1>{input}</h1>
+      <input onChange={onChange} value={input} />
+      <div className="dataFild">
+        {data.map((dt: data, i): JSX.Element => (
+          <div className="card" key={i}>
+            <p>name: {dt.name}</p>
+            <p>email: {dt.username}</p>
+            <p>phone: {dt.phone}</p>
+          </div>
+        ))}
+      </div>
       <Profile name="chan" age={17} />
     </div>
   );
 }
 
-function Profile(props:{name :string, age :number}) :JSX.Element {
+function Profile(props: { name: string, age: number }): JSX.Element {
   return (
     <div>my name is {props.name}.<br />I'm {props.age} years old </div>
   );
