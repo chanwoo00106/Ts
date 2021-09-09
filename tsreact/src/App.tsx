037e0f2bc60421f8b8d251d785dac7176d04169d
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-09 15:30:50 +0900

    useState 사용

--- tsreact/src/App.tsx
+++ tsreact/src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState('teemo');
+
+  const changeName = ():void => {
+    if (user === 'teemo') setUser('cute');
+    else setUser('teemo');
+  }
+  
   return (
     <div className="App">
-      
+      <p>{user}</p>
+      <button onClick={changeName}>Change</button>
+      <Profile name="chan" age={17} />
     </div>
+  );
+}
+
+function Profile(props:{name :string, age :number}) :JSX.Element {
+  return (
+    <div>my name is {props.name}.<br />I'm {props.age} years old </div>
   );
 }
 
