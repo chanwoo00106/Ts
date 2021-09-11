d264e06a28892a847c5b51ba5eeb960c6a040c79
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-11 10:56:46 +0900

    list 형식으로 띄우기 완성

--- tsreact/src/App.tsx
+++ tsreact/src/App.tsx
@@ -14,18 +14,18 @@ interface IState {
 function App() {
   const [people, setPeople] = useState<IState['people']>([{
     name: 'teemo',
-    url: '',
+    url: 'http://pm1.narvii.com/6919/8beca77c10bb36006feea5fb564722621025048dr1-500-393v2_uhq.jpg',
     age: 10,
     note: 'very cute'
   },
   {
     name: 'chan',
-    url: '',
+    url: 'https://www.wallpapertip.com/wmimgs/99-996966_teemo-teemo-wallpaper-phone.jpg',
     age: 17
   },
   {
     name: 'Hyungwoo',
-    url: '',
+    url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1024px-Unofficial_JavaScript_logo_2.svg.png',
     age: 17,
     note: 'studying JS'
   }]);
--- tsreact/src/components/List.tsx
+++ tsreact/src/components/List.tsx
@@ -11,9 +11,18 @@ interface IProps {
 
 const List: React.FC<IProps> = ({ people }) => {
     return (
-        <div>
-
-        </div>
+        <ul>
+            {people.map((person, i) => (
+                <li key={i} className="List">
+                    <div className="List-header">
+                        <img src={person.url} className="List-img" alt="profile" />
+                        <h2>{person.name}</h2>
+                    </div>
+                    <p>{person.age} years old</p>
+                    <p className="List-note">{person.note}</p>
+                </li>
+            ))}
+        </ul>
     )
 }
 
