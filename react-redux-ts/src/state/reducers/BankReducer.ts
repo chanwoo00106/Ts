106af790be3fc92617163e393e5829eeeef00623
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-11 16:55:38 +0900

    ActionType을 정리하고 reducer를 하나로 모음

--- react-redux-ts/src/state/reducers/BankReducer.ts
+++ react-redux-ts/src/state/reducers/BankReducer.ts
@@ -1,29 +1,15 @@
+import { ActionType } from '../action';
+import { Action } from '../actions/index'
 const initialState = 0;
-
-interface DepositAction {
-    type: 'deposit',
-    payload: number
-}
-
-interface WidthrawAction {
-    type: 'widthraw',
-    payload: number
-}
-
-interface BankruptAction {
-    type: 'bankrupt'
-}
-
-type Action = DepositAction | WidthrawAction | BankruptAction;
 
 
 const reducer = (state: number = initialState, action: Action) => {
     switch (action.type) {
-        case "deposit":
+        case ActionType.DEPOSIT:
             return state + action.payload;
-        case "widthraw":
+        case ActionType.WITHDRAW:
             return state - action.payload;
-        case "bankrupt":
+        case ActionType.BANKRUPT:
             return 0;
         default:
             return state;
---
+++ react-redux-ts/src/state/reducers/index.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import bankReducer from './BankReducer'
+
+
+const reducers = combineReducers({
+    bank: bankReducer
+});
+
+export default reducers;
