755dc5fa21496b3dc7da329dd743f122b0ac7327
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-10-06 23:22:19 +0900

    잠시 저장

--- react-shopping-cart/src/App.tsx
+++ react-shopping-cart/src/App.tsx
@@ -1,4 +1,44 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+import { Drawer } from "@material-ui/core";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
+import Badge from "@material-ui/core/Badge";
+
+import { Wrapper } from "./App.styles";
+
+export type CartItemType = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  image: string;
+  category: string;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products',
+    getProducts
+  );
+
+  console.log(data);
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Someting went wrong...</div>
+
   return (
     <div className="App">
       start
---
+++ react-shopping-cart/src/Item/Item.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@material-ui/core";
+
+import { CartItemType } from "../App";
+
+import { Wrapper } from "./Item.styles";
+import React from 'react';
+
+type Props = {
+    item: CartItemType;
+    handleAddToCart: (clickedItem: CartItemType) => void;
+}
+
+const Item: React.FC<Props> = ({ item, handleAddToCart }) => {
+    return (
+        <Wrapper>
+            <img src={item.image} alt={item.title} />
+            <div>
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+                <h3>${item.price}</h3>
+            </div>
+        </Wrapper>
+    )
+}
+
+export default Item;
