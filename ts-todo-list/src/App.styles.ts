5515611764916758bf3b7b59a4035cdc6e810735
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-10-07 23:50:51 +0900

    input 생성과 꾸미기

--- ts-todo-list/src/App.styles.ts
+++ ts-todo-list/src/App.styles.ts
@@ -12,4 +12,39 @@ export const Wrapper = styled.div`
     box-shadow: 0 0 60px rgba(0,0,0,0.1);
     border-radius: 10px;
     padding: 10px;
+
+    h1 {
+        text-align: center;
+        font-size: 2.5rem;
+        margin-bottom: 20px;
+    }
+`;
+
+export const AddTodo = styled.div`
+    display: grid;
+    grid-template-columns: 8fr 2fr;
+    gap: 10px;
+
+    input {
+        min-height: 35px;
+        outline: none;
+        border: none;
+        font-size: 1.5rem;
+        border-bottom: 2px solid #48f592;
+    }
+
+    button {
+        outline: none;
+        border: none;
+        background-color: #48f592;
+        border-radius: 10px;
+        font-size: 1.2rem;
+
+        &:hover{
+            background-color: #2be179;
+        }
+        &:not(:hover){
+            transition: .2s;
+        }
+    }
 `;
--- ts-todo-list/src/App.tsx
+++ ts-todo-list/src/App.tsx
@@ -1,9 +1,13 @@
-import { Wrapper } from './App.styles';
+import { Wrapper, AddTodo } from './App.styles';
 
 function App() {
   return (
     <Wrapper>
-      hello
+      <h1>Ts Todo List</h1>
+      <AddTodo>
+        <input placeholder="할일 입력" type="text" />
+        <button>add</button>
+      </AddTodo>
     </Wrapper>
   );
 }
